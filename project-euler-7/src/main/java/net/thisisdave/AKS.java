c0563b05d2b409555f634b23b41abed246c3faa8
dpmjansen <dpmjansen@gmail.com>
2016-03-11 21:42:25 +0100

    Stuck at stage 2 Should return 29

--- project-euler-7/src/main/java/net/thisisdave/AKS.java
+++ project-euler-7/src/main/java/net/thisisdave/AKS.java
@@ -10,29 +10,38 @@ public class AKS {
         PRIME
     }
 
-    public Number calculateType(int number){
+    public Number calculateType(double number){
         Number returnType = null;
-        isValidInput(number);
+        isValidInput((int)number);
 
         if(isPower(number)){
             return Number.COMPOSITE;
         }
 
 
-        return returnType;
+        return Number.COMPOSITE;
     }
 
 
     /**
-     * Stage one of the AKS Primalisty Test
+     * Stage one of the AKS Primality Test
      * Checks if the given number is a power of another number. If so, the given number must be a Composite number
+     * example:
+     *  If n = a.pow(b) for integers a > 1 and b > 1, output composite.
+     *  For [ b=2, b <= log2(n), b++,
+     *  a=n1/b;
+     *  If [ a is integer, Return[Composite]]
+     *  ];
+     *  a=n1/2...n1/4={5.568, 3.141, 2.360}
+     *
+     *  (From wikipedia)
      *
      * @param number
      * @return true if the number is composite, otherwise it is false
      */
-    private boolean isPower(int number){
+    private boolean isPower(double number){
         double a;
-        for(int b=2; b <= (Math.log(number) / Math.log(2));b++){
+        for(double b=2; b <= (Math.log(number) / Math.log(2));b++){
             a = Math.pow(number,(1/b));
             if(Math.floor(a) == Math.ceil(a)){
                 return true ;
@@ -41,7 +50,29 @@ public class AKS {
         return false;
     }
 
-    private boolean isValidInput(int number){
+    /**
+     * Stage two of AKS. It will return the smallest R value
+     * @param number
+     * @return smallest R
+     */
+    private int smallestR(double number){
+        double maxK = Math.floor(Math.pow((Math.log(number)) / (Math.log(2)),2));
+        double maxR= Math.max(3,Math.ceil(Math.pow(((Math.log(number))/Math.log(2)),5)));
+        int r = 0;
+        double result;
+        boolean nextR = true;
+        for(r=2; nextR && r < maxR; r++){
+            nextR = false;
+            for(int k=1; (!nextR) && (k <= maxK);k++){
+                result = (Math.pow(number,k)) % r;
+                nextR=((result == 1) || (result == 0));
+            }
+        }
+        r--;
+        return r;
+    }
+
+    private boolean isValidInput(int number) {
         if(number < 1){
             throw new ArithmeticException(String.format("The provided number must be 1 or greater. Provided number was: %d",number));
         }
--- project-euler-7/src/test/java/net/thisisdave/AKSTest.java
+++ project-euler-7/src/test/java/net/thisisdave/AKSTest.java
@@ -1,10 +1,13 @@
 package net.thisisdave;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import static org.junit.Assert.fail;
 
 /**
  * Tests for AKS
@@ -18,24 +21,50 @@ public class AKSTest {
         aks = new AKS();
     }
 
-    @Test(expected = ArithmeticException.class)
+    @Test
     public void valueIsInvalid(){
         try {
-            Method method = AKS.class.getDeclaredMethod("isPower");
+            Method method = AKS.class.getDeclaredMethod("isValidInput", int.class);
             method.setAccessible(true);
             method.invoke(aks, -1);
+            fail("Should throw ArithmeticException");
+        } catch (InvocationTargetException e){
+            if(!e.getTargetException().getClass().equals(ArithmeticException.class)){
+                fail("Should throw ArithmeticException");
+            }
+        } catch (IllegalAccessException | NoSuchMethodException e){
+            fail("Should throw ArithmeticException");
+        }
+    }
+
+    /**
+     * Hard to test, but inspection based on other data shows that for n=31 the result should be false.
+     * Furthermore the passthrough value for this method should respectively be {5.568, 3.141, 2.360} which is true, but
+     * only shown when debugging the method. Will work on easier collection of results in the future.
+     */
+    @Test
+    public void numberIsNotPower(){
+        try {
+            Method method = AKS.class.getDeclaredMethod("isPower",double.class);
+            method.setAccessible(true);
+            int number = 31;
+            boolean result = (boolean)method.invoke(aks, number);
+            Assert.assertFalse(String.format("Number is not an power: %d",number),result);
         } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e){
             e.printStackTrace();
         }
     }
 
     @Test
-    public void numberIsPower(){
+    public void smallestR() {
         try {
-            Method method = AKS.class.getDeclaredMethod("isPower");
+            int number = 31;
+            int expectedR = 29;
+            Method method = AKS.class.getDeclaredMethod("smallestR", double.class);
             method.setAccessible(true);
-            method.invoke(aks, 1);
-        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e){
+            int smallestR = (int) method.invoke(aks, number);
+            System.out.println(smallestR);
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
             e.printStackTrace();
         }
     }
